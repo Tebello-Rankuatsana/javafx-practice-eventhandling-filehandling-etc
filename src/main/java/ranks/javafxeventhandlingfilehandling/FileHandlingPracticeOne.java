80c5a2f5db00d1ffede78c02ca5c0ae94cfb8cae
Tebello Rankuatsana <justranku@gmail.com>
2026-03-13 20:09:30 +0200

    Creating a controller to handle login file handlng operations

---
+++ src/main/java/ranks/javafxeventhandlingfilehandling/FileHandlingPracticeOne.java
@@ -0,0 +1,32 @@
+package ranks.javafxeventhandlingfilehandling;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class FileHandlingPracticeOne {
+
+    @FXML
+    private PasswordField PsdPassword;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Label lblPassword;
+
+    @FXML
+    private Label lblUsername;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void handleLogin(ActionEvent event) {
+
+    }
+
+}
