b66dd7fc7d4801279f0945e23349420704cef905
Tebello Rankuatsana <justranku@gmail.com>
2026-03-13 20:16:16 +0200

    Controller to handle saving to a file

---
+++ src/main/java/ranks/javafxeventhandlingfilehandling/BasicFileHandlingOperations.java
@@ -0,0 +1,21 @@
+package ranks.javafxeventhandlingfilehandling;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class BasicFileHandlingOperations {
+
+    @FXML
+    private Button btnEnter;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void saveName(ActionEvent event) {
+
+    }
+
+}
